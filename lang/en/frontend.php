43cefd5633b314ec2f9636b19b4bb25a42e17c2d
Total VDO Solution <125175458+TotalVDO123@users.noreply.github.com>
2026-08-18 17:35:33 +0530

    Update frontend.php

--- lang/en/frontend.php
+++ lang/en/frontend.php
@@ -57,10 +57,7 @@ return [
     'movies' => 'Movies',
     'movie' => 'Movie',
     'tvshows' => 'TV Shows',
-<<<<<<< HEAD
-=======
     'embeded_tvshows' => 'Episodes',
->>>>>>> 725da5b48051e0a70583fc8dc361d18ce44ee6ee
     'tvshow' => 'TV Show',
     'watch_now' => 'Watch now',
     'rate_this' => 'Rate this',
